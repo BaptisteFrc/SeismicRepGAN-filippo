a3ab7419edf2cc091f99587b3783cb2918fa7aef
Mathilde__5 <124593697+MathildeMorhain@users.noreply.github.com>
2024-02-06 12:47:44 +0100

    Update models_tested.py

--- models_tested.py
+++ models_tested.py
@@ -286,3 +286,81 @@ class linear_decoder(torch.nn.Module):
         x = self.linear5(x)
         x = np.swapaxes(x,i,j)
         return x
+
+#Definition of LSTM layer
+def c_lstm(input_size, hidden_size) :
+  return torch.nn.LSTM(input_size=input_size, hidden_size=hidden_size,num_layers=1,batch_first=True,dropout=0.0)
+
+#Encoder with LSTM layers
+
+class lstm_encoder(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.lstm1 = c_lstm(161, 128)
+        self.lstm2 = c_lstm(128, 100)
+        self.lstm3 = c_lstm(100, 80)
+        self.lstm4 = c_lstm(80, 60)
+        self.lstm5 = c_lstm(60, 50)
+
+    def reset_parameters(self):
+        list_layer=[self.lstm1,self.lstm2,self.lstm3,self.lstm4,self.lstm5]
+        for layer in list_layer:
+            if hasattr(layer, 'reset_parameters'):
+                layer.reset_parameters()
+
+    def forward(self, x):
+        if len(x.size())==3 :
+          i,j=1,2
+        else :
+          i,j=0,1
+        x = np.swapaxes(x,i,j)
+        x,ras = self.lstm1(x)
+        x = torch.nn.functional.relu(x)
+        x,ras = self.lstm2(x)
+        x = torch.nn.functional.relu(x)
+        x,ras = self.lstm3(x)
+        x = torch.nn.functional.relu(x)
+        x,ras = self.lstm4(x)
+        x = torch.nn.functional.relu(x)
+        x,ras = self.lstm5(x)
+        x = torch.nn.functional.relu(x)
+        x = np.swapaxes(x,i,j)
+        return x
+
+#Decoder with LSTM layers
+
+class lstm_decoder(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.lstm1 = c_lstm(50, 60)
+        self.lstm2 = c_lstm(60, 80)
+        self.lstm3 = c_lstm(80, 100)
+        self.lstm4 = c_lstm(100, 128)
+        self.lstm5 = c_lstm(128, 241)
+
+    def reset_parameters(self):
+        list_layer=[self.lstm1,self.lstm2,self.lstm3,self.lstm4,self.lstm5]
+        for layer in list_layer:
+            if hasattr(layer, 'reset_parameters'):
+                layer.reset_parameters()
+
+    def forward(self, x):
+        if len(x.size())==3 :
+          i,j=1,2
+        else :
+          i,j=0,1
+        x = np.swapaxes(x,i,j)
+        x,ras = self.lstm1(x)
+        x = torch.nn.functional.relu(x)
+        x,ras = self.lstm2(x)
+        x = torch.nn.functional.relu(x)
+        x,ras = self.lstm3(x)
+        x = torch.nn.functional.relu(x)
+        x,ras = self.lstm4(x)
+        x = torch.nn.functional.relu(x)
+        x,ras = self.lstm5(x)
+        x = np.swapaxes(x,i,j)
+        return x
+
